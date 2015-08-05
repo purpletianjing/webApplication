f74b52531f3d556811e1b455872e769048b09b9a
purple <purpletianjing@163.com>
2015-08-05 15:17:49 +0800

    add pour-into-data

--- spec/lib/mutil-choose-spec.js
+++ spec/lib/mutil-choose-spec.js
@@ -1,6 +1,14 @@
 describe("mutilChoose test",function() {
+  var answer = [];
+  beforeEach(function() {
+  answer = [{name:'1-1',value:['A'],score:3},
+                  {name:'2-1',value:['B'],score:3},
+                  {name:'3-1',value:['A','B','C','D'],score:3}];
+
+  });
   var theMutilChoose = new MutilChoose('3-1','checkbox',['A','B','C','D']);
   it("check name,value and type",function() {
+    MutilChoose.getCorrectAnswer(answer);
     expect(theMutilChoose.name).toBe('3-1');
     expect(theMutilChoose.type).toBe('checkbox');
     expect(theMutilChoose.value).toEqual(['A','B','C','D']);
