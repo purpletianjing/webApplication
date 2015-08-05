badb694beb0ca22118c5087fd98c29c690ed3df9
purple <purpletianjing@163.com>
2015-08-05 17:01:04 +0800

    add mutil-blank

--- spec/lib/mutil-fill-blank-spec.js
+++ spec/lib/mutil-fill-blank-spec.js
@@ -1,6 +1,17 @@
 describe("mutilFillBlank test",function() {
-  var mutilFill = new MutilFill(name,type,value);
+  var answer;
+  var mutilFill;
+  beforeEach(function() {
+    mutilFill = new MutilFill('2-1','text',['多态','继承','多态']);
+    answer = [{name:'2-1',type:'text',value:['多态','继承','封装']}];
+  });
   it("check his name,type and value",function() {
-    
+    expect(mutilFill.name).toBe('2-1');
+    expect(mutilFill.type).toBe('text');
+    expect(mutilFill.value).toEqual(['多态','继承','多态']);
+  });
+  it("get score",function(){
+    MutilFill.getCorrectAnswer(answer);
+    expect(mutilFill.getScore()).toBe(2);
   });
 });
